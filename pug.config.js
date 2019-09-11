b3eac3529321a9465ed230561225c78e3eb240a7
Ivens <ivensjorisnbacon@gmail.com>
2019-09-11 00:50:01 -0300

    about icons added with flip card

--- pug.config.js
+++ pug.config.js
@@ -3,6 +3,9 @@ module.exports = {
       "home_text": "Desta maneira, a competitividade nas transações comerciais garante a contribuição de um grupo importante na determinação do remanejamento dos quadros funcionais.",
       "about": "A nível organizacional, o novo modelo estrutural aqui preconizado acarreta um processo de reformulação e modernização das formas de ação.",
       "about2": "Evidentemente, a complexidade dos estudos efetuados desafia a capacidade de equalização das posturas dos órgãos dirigentes com relação às suas atribuições.",
-      "about3": "Gostaria de enfatizar que o julgamento imparcial das eventualidades nos obriga à análise dos relacionamentos verticais entre as hierarquias."
+      "about3": "Gostaria de enfatizar que o julgamento imparcial das eventualidades nos obriga à análise dos relacionamentos verticais entre as hierarquias.",
+      "missao": "Pensando mais a longo prazo, o julgamento imparcial das eventualidades afeta positivamente a correta previsão do orçamento setorial.",
+      "visao": "Todavia, a complexidade dos estudos efetuados causa impacto indireto na reavaliação do sistema de formação de quadros que corresponde às necessidades.",
+      "valores": "No mundo atual, a determinação clara de objetivos aponta para a melhoria de alternativas às soluções ortodoxas.",
     }
 };
